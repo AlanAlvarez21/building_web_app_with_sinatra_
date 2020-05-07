ec60fef631880bc5f1395ed5a9b1de3d64ed0d32
AlanAlvarez21 <alandanielalvarez0000@gmail.com>
2020-05-07 15:33:19 -0700

    ERB Tags implementation

--- wiki.rb
+++ wiki.rb
@@ -2,8 +2,27 @@ require 'sinatra'
 
 set :bind, "0.0.0.0"
 
+def page_content(title)           #esté metódo carga el contenido de un text file y lo returno como string, pasas el pages title como argumento
+  File.read("pages/#{title}.txt") #si no se encuentra el file.read
+rescue Errno::ENOENT              # Erno ENOENT exception will raise
+  return nil                      #Return Nil Many methods can return nil as a result, This happens when you ask for a value but that value is not available.
+end
 
 
-get "/" do
+get "/" do                        # calls the erb welcome file
  erb :welcome
+end
+
+
+get "/test" do                    #call test.erb, is a regular embebed code
+ erb :test
+end
+
+
+get "/demo" do
+  erb :demo
+end
+
+get "/:title" do                  #the path to make match he url parameter, parameters are marked with a colon followed by a parameter name
+page_content(params[:title])
 end
